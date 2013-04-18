7ddd93a575dd25db51f008954879fac0fc8c5c90
CT Arrington <ct@nowhere.com>
2013-04-18 13:41:25 -0400

    initial

---
+++ LanguageSandbox/OldFeaturesAndTests/OldFeaturesAndTests/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OldFeaturesAndTests
+{
+    class Person
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+        }
+
+        public Person(string firstName = "John", string lastName = "Doe")
+        {
+            // TODO: Complete member initialization
+            this._firstName = firstName;
+            this._lastName = lastName;
+        }        
+    }
+}
---
+++ LanguageSandbox/OldFeaturesAndTests/OldFeaturesAndTests/PersonTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OldFeaturesAndTests
+{
+    [TestClass]
+    public class PersonTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Person joe = new Person("Joe", "Smith");
+            Assert.AreEqual("Joe", joe.FirstName);
+            Assert.AreEqual("Smith", joe.LastName);
+
+            Person backwards = new Person(lastName: "htimS", firstName: "eoJ");
+            Assert.AreEqual("eoJ", backwards.FirstName);
+            Assert.AreEqual("htimS", backwards.LastName);
+
+            Person nobody = new Person();
+            Assert.AreEqual("John", nobody.FirstName);
+            Assert.AreEqual("Doe", nobody.LastName);
+
+            Person nfn = new Person(lastName: "Smith");
+            Assert.AreEqual("John", nfn.FirstName);
+            Assert.AreEqual("Smith", nfn.LastName);
+        }
+    }
+}
