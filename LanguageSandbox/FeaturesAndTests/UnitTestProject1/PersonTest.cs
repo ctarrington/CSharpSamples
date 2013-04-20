cf880ced87a2bd24a8a38a351c2b74462f2b49f7
CT Arrington <ct@nowhere.com>
2013-04-20 12:04:28 -0400

    messing with lists

--- LanguageSandbox/FeaturesAndTests/UnitTestProject1/PersonTest.cs
+++ LanguageSandbox/FeaturesAndTests/UnitTestProject1/PersonTest.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Text;
 
 namespace UnitTestProject1
 {
@@ -27,5 +30,37 @@ namespace UnitTestProject1
 
             // nfn.FirstName = "Ted";  compiler error because setters are private
         }
+
+        [TestMethod]
+        public void TestLists()
+        {
+            List<Person> people = new List<Person> { new Person(), new Person("Ted", "TheDog"), new Person("Fred", "TheDog"), new Person("Joe", "Schmoo") };
+            Assert.AreEqual("Joe", people[3].FirstName);
+            Assert.AreEqual("Schmoo", people[3].LastName);
+            Assert.AreEqual(4, people.Count);
+
+            List<Person> theJoes = people.FindAll(p => p.FirstName.Equals("Joe"));
+            Assert.AreEqual(1, theJoes.Count);
+
+            theJoes.Add(new Person("Joe", "Soe"));
+            Assert.AreEqual(2, theJoes.Count);
+            Assert.AreEqual(4, people.Count);
+
+            var joeschmoo = theJoes.FindAll(p => p.FirstName.Equals("Joe") && p.LastName.Equals("Schmoo"));
+            Assert.AreEqual(1, joeschmoo.Count);
+
+            var sando = theJoes.FindAll(p => p.FirstName.Equals("Joe") && Regex.IsMatch(p.LastName, "S.*o.*"));
+            Assert.AreEqual(2, sando.Count);
+
+            StringBuilder sb = new StringBuilder();
+            foreach (Person person in people)
+            {
+                sb.Append(String.Format("{0}, {1}\n", person.LastName, person.FirstName));
+            }
+
+            string pretty = sb.ToString();
+            Assert.AreEqual("Doe, John\nTheDog, Ted\nTheDog, Fred\nSchmoo, Joe\n", pretty);
+        }
+
     }
 }
